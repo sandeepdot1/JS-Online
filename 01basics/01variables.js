c7348b292bf38b7145d9e0131def2a9290e535b1
Sandeep Kumar Panigrahi <sandeeppanigrahi3570@gmail.com>
2024-05-21 19:48:44 +0000

    Added 01variables.js

---
+++ 01basics/01variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "sandeep@email.com"
+var accountPassword = "12345"
+accountCity = "Hyderabad"
+let accountState
+
+
+accountEmail = "s@gmail.com"
+accountPassword = "abcd"
+accountCity = "Delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
