05edad67c0834edcc8afff000ae499287dae5741
Sandeep Kumar Panigrahi <sandeeppanigrahi3570@gmail.com>
2024-05-22 16:59:51 +0000

    Added 02dataconversion.js

---
+++ 01basics/02dataconversion.js
@@ -0,0 +1,26 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueNumber = Number(score)
+
+console.log(typeof valueNumber)
+console.log(valueNumber)
+
+// "33" => 33
+// "33A" => NaN
+// undefined => NaN
+// null => 0
+
+let isLoggedIn = 10
+console.log(typeof isLoggedIn)
+
+let boolIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof isLoggedIn)
+console.log(boolIsLoggedIn)
+
+
+// 1 => true; 0 => false
+// "" => false
+// "sandeep" => true
+// null => false; undefined => false
