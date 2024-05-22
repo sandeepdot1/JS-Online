0c6f05b017f81481d6af383165ac4a63582da001
Sandeep Kumar Panigrahi <sandeeppanigrahi3570@gmail.com>
2024-05-22 17:55:45 +0000

    Added 03operations.js

---
+++ 01basics/03operations.js
@@ -0,0 +1,17 @@
+let value = 3
+let negValue = -value
+
+console.log(value)
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(3/2) // actual divide
+console.log(3%2)
+
+str1 = "Hello "
+str2 = "Sandeep"
+
+console.log(str1 + str2)
